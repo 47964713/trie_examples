b0f9556cef7fa6bcfa599398dc6b9f7cc3a03593
Tim Olson <timo@smu.edu>
2021-04-03 14:56:07 -0500

    CMake and project configuration

---
+++ cmake-build-debug/CMakeFiles/trie_examples.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trie_examples.dir/main.cpp.o"
+  "trie_examples"
+  "trie_examples.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trie_examples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
